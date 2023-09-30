ad97ce2ef0de5d54feb7286b4a02bfcc791aaba2
Mateo Santa <mateo.santamanrique@gmail.com>
2023-09-30 14:05:02 +0200

    POST create one user & PUT edit one specific  user by id

--- users.js
+++ users.js
@@ -36,6 +36,33 @@ usersRouter.get("/:id", async (req, res) => {
     }
 })
 
+usersRouter.post("/", async (req, res) => {
+    const { first_name, last_name, age } = req.body;
+    try {
+        const result = await pool.query('INSERT INTO users (first_name, last_name, age) VALUES ($1, $2, $3) RETURNING *;', [first_name, last_name, age]);
+        res.json(result.rows)
+
+    } catch(err){
+        res.status(500).json(err)
+    }
+
+})
+
+usersRouter.put("/:id", async (req, res) => {
+    const id = req.params.id;
+    const { first_name, last_name, age } = req.body;
+    try {
+        const result = await pool.query('UPDATE users SET first_name=$1, last_name=$2, age=$3 WHERE id=$4  RETURNING *;', [first_name, last_name, age, id]);
+        res.json(result.rows)
+
+    } catch(err){
+        res.status(500).json(err)
+    }
+
+})
+
+
+
 
 
 
