69b4ced9f70206c6b5f1210dc3e372959fa8a85f
Mateo Santa <mateo.santamanrique@gmail.com>
2023-09-30 14:03:15 +0200

    GET all users

---
+++ users.js
@@ -0,0 +1,30 @@
+import express from 'express';
+const usersRouter = express.Router();
+import pg from 'pg';
+
+const {Pool} = pg;
+
+const pool = new Pool({
+    user: process.env.PGUSER,
+    host: process.env.PGHOST,
+    database: process.env.PGDATABASE,
+    password: process.env.PGPASSWORD,
+    port: process.env.PGPORT,
+  })
+  //can also write: const pool = new Pool(), library recognizes .env content
+
+
+
+usersRouter.get("/", async (req, res) => {
+    try {
+        const result = await pool.query('SELECT * FROM users');
+        res.json(result.rows)
+
+    } catch(err){
+        res.status(500).json(err)
+    }
+})
+
+
+
+export default usersRouter;
